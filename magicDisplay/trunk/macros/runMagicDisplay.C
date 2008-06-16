eaca0109b33921b2041258b4920ae60222e0d537
rjn <rjn@d90a992a-4ffb-0310-9b0e-ce574b03d632>
2008-06-16 19:36:59 +0000

    Tidying up Makefile

--- magicDisplay/trunk/macros/runMagicDisplay.C
+++ magicDisplay/trunk/macros/runMagicDisplay.C
@@ -15,7 +15,7 @@ void runMagicDisplay() {
 
   TChain *fred=0; //Will this work?
 
-  MagicDisplay *magicPtr = new MagicDisplay("/home/anita/Desktop",2297);
+  MagicDisplay *magicPtr = new MagicDisplay("/home/rjn/anita/data/testing/",2297);
   magicPtr->startSurfDisplay();
   magicPtr->startTurfDisplay();
   magicPtr->startEventDisplay();
