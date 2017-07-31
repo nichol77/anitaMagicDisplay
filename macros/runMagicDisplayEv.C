be874c4599468598c6287c5460f1ed3bacc93efb
Ben Rotter <BenJRotter@gmail.com>
2017-07-31 10:55:03 -1000

    Added a macro I've been using that opens MagicDisplay with just an event number, so you don't need to know both eventNumber and run.  Only works for ANITA3 for now until someone makes a runToEvA4.txt list

---
+++ macros/runMagicDisplayEv.C
@@ -0,0 +1,68 @@
+#include "AnitaConventions.h"
+#include "AnitaVersion.h"
+
+int evToRun(int ev) {
+
+  char* installDir = getenv("ANITA_UTIL_INSTALL_DIR");
+
+  stringstream name;
+  ifstream runToEv;
+  if (AnitaVersion::get() == 3) {
+    name.str("");
+    name << installDir << "/bin/runToEvA3.txt";
+    cout << "Using: " << name.str() << " as translation file" << endl;
+    runToEv.open(name.str().c_str());
+    if (!runToEv.is_open()) {
+      cout << "I couldn't open that file though sorry!" << endl;
+      return -1;
+    }
+
+  }
+  else {
+    cout << "I only have a runToEv list for Anita3 right now sorry :(" << endl;
+    return -1;
+  }
+
+  int run,evLow,evHigh;
+  int runOut = -1;
+  while (runToEv >> run >> evLow >> evHigh)
+    if (ev >= evLow && ev <= evHigh) {
+      runOut = run;
+    }
+
+  runToEv.close();
+  
+  return runOut;
+
+}
+
+
+
+void runMagicDisplayEv(int ev) {
+
+  cout << "Using AnitaVersion " << AnitaVersion::get() << endl;
+
+  int run = evToRun(ev);
+  if (run == -1) {
+    cout << "Couldn't relate event to run :( Quitting" << endl;
+    return;
+  }
+
+  cout << "Event number " << ev << " is in run " << run << endl;
+
+  stringstream dirName;
+  if (run>10000) {
+    dirName << "/Volumes/ANITA3Data/antarctica14/root";
+  }
+  else {
+    dirName << "/Volumes/ANITA3Data/root";
+  }
+
+  MagicDisplay *magicPtr = new MagicDisplay(dirName.str().c_str(),run,WaveCalType::kDefault);
+
+  magicPtr->startEventDisplay();
+
+  magicPtr->displayThisEvent(ev,run); 
+
+
+}
